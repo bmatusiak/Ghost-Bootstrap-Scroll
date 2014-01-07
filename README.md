8542b35b6715f98f777fdc626f4f17d4f92fbc88
Bradley Matusiak <usabrad86@bellsouth.net>
2014-01-06 19:46:21 -0500

    init release

---
+++ README.md
@@ -0,0 +1,8 @@
+Bootstrap-Scroll
+=======
+
+Theme for the [Ghost Blogging platform](http://ghost.org "Ghost Blogging Platform")
+
+Visit Ghost at: [www.ghost.org](http://www.ghost.org "Ghost")
+
+![Screenshot](ghost.png?raw=true)
---
+++ assets/js/index.js
@@ -0,0 +1,23 @@
+/**
+ * Main JS file for Casper behaviours
+ */
+
+/*globals jQuery, document */
+(function ($) {
+    "use strict";
+
+    $(document).ready(function(){
+
+        // On the home page, move the blog icon inside the header 
+        // for better relative/absolute positioning.
+
+        //$("#blog-logo").prependTo("#site-head-content");
+        hljs.initHighlightingOnLoad();
+        
+        $('[data-spy="scroll"]').each(function () {
+            var $spy = $(this).scrollspy('refresh')
+        })
+        
+    });
+
+}(jQuery));
