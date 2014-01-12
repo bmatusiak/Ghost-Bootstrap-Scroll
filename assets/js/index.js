7b71f1cc82a942263ecf4fbefc0f0ffc5998e41d
Bradley Matusiak <usabrad86@bellsouth.net>
2014-01-12 14:11:50 -0500

    added ace

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,26 +12,58 @@
         // for better relative/absolute positioning.
 
         //$("#blog-logo").prependTo("#site-head-content");
-        window.hljs.initHighlightingOnLoad();
+        //window.hljs.initHighlightingOnLoad();
+        
         
         $('[data-spy="scroll"]').each(function (i,item) {
             $(item).scrollspy('refresh');
         });
         
-        $(".mainbody").scroll(function() {
+        /*
+            $(".mainbody").scroll(function() {
             
-            var height = $(".main-content").innerHeight()-$(".mainbody").innerHeight();
-            var sideHeight = $("#side-nav").outerHeight(true);
-            var curScrollPOS = $(".mainbody").scrollTop();
-            var pos = curScrollPOS - height;
+                var height = $(".main-content").innerHeight()-$(".mainbody").innerHeight();
+                var sideHeight = $("#side-nav").outerHeight(true);
+                var curScrollPOS = $(".mainbody").scrollTop();
+                var pos = curScrollPOS - height;
+                
+                if (pos > 0) {
+                    var hasRoom = pos < $(".mainbody").innerHeight() - sideHeight;
+                    if(!hasRoom)
+                    $("#side-nav").css("bottom",pos);
+                }else{
+                    $("#side-nav").css("bottom","");
+                }
+            });
+        */
+        //$("pre code").each(function(i, eValue) {
+        //    $(eValue).css("height", $(eValue).height());
+        //});
+        $("pre code").each(function(i, eValue) {
+            $(eValue).css("height", $(eValue).height());
             
-            if (pos > 0) {
-                var hasRoom = pos < $(".mainbody").innerHeight() - sideHeight;
-                if(!hasRoom)
-                $("#side-nav").css("bottom",pos);
-            }else{
-                $("#side-nav").css("bottom","");
-            }
+            var mode = $(eValue).attr("class");
+            
+            var editor = window.ace.edit(eValue);
+            
+            editor.setHighlightActiveLine(false);
+            editor.setReadOnly(true);
+            editor.setShowFoldWidgets(false);
+            
+            var session = editor.getSession();
+            if(mode)
+                session.setMode("ace/mode/"+mode);
+            
+            var count = session.getLength();
+            
+            setTimeout(function(){
+                var len = count + 1;
+                var hei = 14;
+                console.log(count,"x",hei,len * hei);
+                
+                $(eValue).css("height",(len * hei)+5);
+            });
+            
         });
         
     });
